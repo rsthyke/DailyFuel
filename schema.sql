77cb4e90b367b31e5d3654cc506db96281ae92f0
Raşit Akyürek <ndndrasit@gmail.com>
2026-05-14 21:50:36 +0300

    add meals table to database

--- schema.sql
+++ schema.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE IF NOT EXISTS users (
     id           INTEGER PRIMARY KEY AUTOINCREMENT,
     name         TEXT    NOT NULL,
@@ -9,4 +8,12 @@ CREATE TABLE IF NOT EXISTS users (
     age          INTEGER,
     gender       TEXT,
     calorie_goal REAL DEFAULT 2000
+);
+
+CREATE TABLE IF NOT EXISTS meals (
+    id        INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id   INTEGER NOT NULL REFERENCES users(id),
+    date      TEXT    NOT NULL,
+    meal_name TEXT    NOT NULL,
+    calories  INTEGER NOT NULL
 );
